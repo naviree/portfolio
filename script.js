1ac8f98acd3841ac3d586b40f5fe5f84761c309e
Jeremy Jefferson <jeremyijefferson@gmail.com>
2024-01-03 08:36:42 -0800

    Update portfolio title and project descriptions

--- script.js
+++ script.js
@@ -2,55 +2,55 @@ const totalLines = 24;
 let initialized = false;
 
 const randInt = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 function makePath() {
-    let startY = randInt(5, 15);
-    let path = `M-${100 / totalLines} ${startY}v${-startY}`;
+	let startY = randInt(5, 15);
+	let path = `M-${100 / totalLines} ${startY}v${-startY}`;
 
-    for (let i = 0; i < totalLines + 2; i++) {
-        let y = randInt(-2, 2);
-        path += `l${100 / totalLines} ${y}`;
-    }
-    path += `v40z`;
-    return path;
+	for (let i = 0; i < totalLines + 2; i++) {
+		let y = randInt(-2, 2);
+		path += `l${100 / totalLines} ${y}`;
+	}
+	path += `v40z`;
+	return path;
 }
 
 function go() {
-    if (!initialized) {
-        for (let i = 0; i < totalLines; i++) {
-            let p = document.createElementNS(
-                "http://www.w3.org/2000/svg",
-                "path"
-            );
-            p.id = `p${i}`;
-            p.setAttribute("d", makePath());
-            p.setAttribute("opacity", (i + 1) / totalLines);
-            p.setAttribute(
-                "transform",
-                `translate(${(-i / totalLines) * 50} ${
-                    50 + (50 / totalLines) * i
-                }) scale(${1 + i / totalLines})`
-            );
-            document.querySelector("#mtns").appendChild(p);
-        }
-        initialized = true;
-    }
+	if (!initialized) {
+		for (let i = 0; i < totalLines; i++) {
+			let p = document.createElementNS(
+				"http://www.w3.org/2000/svg",
+				"path"
+			);
+			p.id = `p${i}`;
+			p.setAttribute("d", makePath());
+			p.setAttribute("opacity", (i + 1) / totalLines);
+			p.setAttribute(
+				"transform",
+				`translate(${(-i / totalLines) * 50} ${
+					50 + (50 / totalLines) * i
+				}) scale(${1 + i / totalLines})`
+			);
+			document.querySelector("#mtns").appendChild(p);
+		}
+		initialized = true;
+	}
 
-    if (initialized) {
-        for (let i = totalLines - 1; i >= 0; i--) {
-            if (document.querySelector(`#p${i - 1}`)) {
-                let pathUp = document
-                    .querySelector(`#p${i - 1}`)
-                    .getAttribute("d");
-                document.querySelector(`#p${i}`).setAttribute("d", pathUp);
-            }
-        }
+	if (initialized) {
+		for (let i = totalLines - 1; i >= 0; i--) {
+			if (document.querySelector(`#p${i - 1}`)) {
+				let pathUp = document
+					.querySelector(`#p${i - 1}`)
+					.getAttribute("d");
+				document.querySelector(`#p${i}`).setAttribute("d", pathUp);
+			}
+		}
 
-        // Generate a new random path for the top path
-        document.querySelector(`#p0`).setAttribute("d", makePath());
-    }
+		// Generate a new random path for the top path
+		document.querySelector(`#p0`).setAttribute("d", makePath());
+	}
 }
 
 go();
